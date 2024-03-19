3acfb32abfb3ac07ae3c6d4ece87fa1646777442
deadcxlm <deadcalm11kata@gmail.com>
2024-03-19 17:20:03 +0700

    Implemented interfaces for controller

--- TomagotchiGame/Controllers/ITomagotchiController.cs
+++ TomagotchiGame/Controllers/ITomagotchiController.cs
@@ -9,5 +9,9 @@ namespace TomagotchiGame.Controllers
     public interface ITomagotchiController
     {
         Task StartGame();
+        Task SaveGame(string fileName);
+        Task LoadGame(string fileName);
+        Task MainMenu();
+        Task StartNewGame();
     }
 }
